543d21d77d6b2556e0bd938433a2ca5f2982d18e
sidmall <s.malla@qub.ac.uk>
2023-05-26 16:39:41 +0100

    Added highMeanGene function

--- R/PDSpredict.R
+++ R/PDSpredict.R
@@ -127,22 +127,3 @@ PDSpredict <- function(x, species = c("human", "mouse"), threshold = 0.6) {
   return(PDS.predict)
 
 }
-
-
-
-highMeanGene <- function (df) {
-  x.df <- as.data.frame(df)
-  x.df <- dplyr::mutate(x.df, gene_noString = gsub("\\s.*",
-                                                   "", x.df[, 1]))
-  x.df <- dplyr::select(x.df, "gene_noString", dplyr::everything())
-  x.df <- dplyr::mutate(x.df, mean = apply(x.df[, -c(1:2)],
-                                           1, mean))
-  x.df <- dplyr::select(x.df, "mean", dplyr::everything())
-  x.df <- x.df[order(x.df[["mean"]], decreasing = T), ]
-  x.df <- x.df[-which(duplicated(x.df[["gene_noString"]])),
-  ]
-  x.mtx <- as.matrix(x.df[, -c(1:3)])
-  rownames(x.mtx) <- x.df[["gene_noString"]]
-  x.mtx <- x.mtx[order(rownames(x.mtx), decreasing = F), ]
-  return(x.mtx)
-}
